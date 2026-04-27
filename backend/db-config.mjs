b13d00670bf9f04b4ae00cc11385438596e638d9
Jason Reevs <jasonrvs88@gmail.com>
2026-04-27 17:25:26 -0400

    Delete backend/db-config.mjs

--- backend/db-config.mjs
+++
@@ -1,9 +0,0 @@
-export default function getDbConfig() {
-    return {
-        host: process.env.DB_HOST || 'localhost',
-        port: process.env.DB_PORT || 5432,
-        user: process.env.DB_USER || 'user',
-        password: process.env.DB_PASSWORD || 'password',
-        database: process.env.DB_NAME || 'database',
-    };
-}
